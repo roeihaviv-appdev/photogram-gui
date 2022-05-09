5ca67b2ee7ff4e1d013e6553dedd76f5f3841a1a
roeihaviv <rhaviv@chicagobooth.edu>
2022-05-09 14:59:51 +0000

    photos started (1 hr in video)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   get("/users/:path_username", { :controller => "users", :action => "show" })
 
+  get("/photos", { :controller => "photos", :action => "index" })
+
 
 end
