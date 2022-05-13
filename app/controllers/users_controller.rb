522f3a3faaf41fa4f6f52c6976b77af31a7f4732
roeihaviv <rhaviv@chicagobooth.edu>
2022-05-13 14:10:49 +0000

    GUI complete - 100%

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   def index
     matching_users = User.all
     @list_of_users = matching_users.order ({ :username => :asc })
+
+    # url_username = params.fetch("path_username")
+    # matching_usernames = User.where({ :username => url_username })
+    # @the_user = matching_usernames.at(0)
+    # @the_user.save
    
     render({ :template => "user_templates/index.html.erb" })
   end
@@ -42,13 +47,15 @@ class UsersController < ApplicationController
 
     input_username = params.fetch("input_username")
 
-    a_new_user = User.new
+    the_id = params.fetch("modify_id")
 
-    a_new_user.username = input_username
+    edit_user = User.where({ :id => the_id }).at(0)
 
-    a_new_user.save
+    edit_user.username = input_username
 
-    next_url = "/users/" + a_new_user.username.to_s
+    edit_user.save
+
+    next_url = "/users/" + edit_user.username.to_s
 
     redirect_to(next_url)
   
